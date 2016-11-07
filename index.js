32f87ac766d834c89ff2eeda352d6a3403d6f578
Diandra Ryan-Mas <Ryan-Mas_Diandra@bah.com>
2016-11-07 14:24:36 -0600

    added favicon

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
+
 app.use(express.static('public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.set('port', (process.env.PORT || 5000));
 
