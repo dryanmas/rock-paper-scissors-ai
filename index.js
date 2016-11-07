7f4f3507e907faf2db1c9e92b49ed24b71135118
Diandra Ryan-Mas <Ryan-Mas_Diandra@bah.com>
2016-11-06 21:24:21 -0600

    I think I figured it out

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/public/index.html')
 });
 
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
